7487a8a41ab051e773c49f3eaacf8a6addcb83fe
Ruchi Reddy <ruchireddy@Ruchis-MacBook-Pro.local>
2022-11-06 13:17:33 -0500

    commit

--- src/tuiter/navigation-sidebar/index.js
+++ src/tuiter/navigation-sidebar/index.js
@@ -11,42 +11,42 @@ const NavigationSidebar = (
                 <i className="fab fa-twitter"></i>
             </a>
             <a className={`list-group-item
-                    ${active === 'home'?'active':''}`} href={}>
+                    ${active === 'home'?'active':''}`} href="">
                 <i className="fa fa-home"></i> <span
                 className="d-sm-none d-md-none d-lg-none d-xl-block">Home</span>
             </a>
             <a className={`list-group-item
-                    ${active === 'explore'?'active':''}`} href={}>
+                    ${active === 'explore'?'active':''}`} href="">
                 <i className="fa fa-glasses"></i> <span
                 className="d-sm-none d-md-none d-lg-none d-xl-block">Explore</span>
             </a>
             <a className={`list-group-item
-                    ${active === 'notifications'?'active':''}`} href={}>
+                    ${active === 'notifications'?'active':''}`} href="">
                 <i className="fa fa-bell"></i> <span
                 className="d-sm-none d-md-none d-lg-none d-xl-block">Notifications</span>
             </a>
             <a className={`list-group-item
-                    ${active === 'messages'?'active':''}`} href={}>
+                    ${active === 'messages'?'active':''}`} href="">
                 <i className="fa fa-comment"></i> <span
                 className="d-sm-none d-md-none d-lg-none d-xl-block">Messages</span>
             </a>
             <a className={`list-group-item
-                    ${active === 'bookmarks'?'active':''}`} href={}>
+                    ${active === 'bookmarks'?'active':''}`} href="">
                 <i className="fa fa-bookmark"></i> <span
                 className="d-sm-none d-md-none d-lg-none d-xl-block">Bookmarks</span>
             </a>
             <a className={`list-group-item
-                    ${active === 'lists'?'active':''}`} href={}>
+                    ${active === 'lists'?'active':''}`} href="">
                 <i className="fa fa-list"></i> <span
                 className="d-sm-none d-md-none d-lg-none d-xl-block">Lists</span>
             </a>
             <a className={`list-group-item
-                    ${active === 'profile'?'active':''}`} href={}>
+                    ${active === 'profile'?'active':''}`} href="">
                 <i className="fa fa-person-booth"></i> <span
                 className="d-sm-none d-md-none d-lg-none d-xl-block">Profile</span>
             </a>
             <a className={`list-group-item
-                    ${active === 'more'?'active':''}`} href={}>
+                    ${active === 'more'?'active':''}`} href="">
                 <i className="fa fa-plus"></i> <span
                 className="d-sm-none d-md-none d-lg-none d-xl-block">More</span>
             </a>
